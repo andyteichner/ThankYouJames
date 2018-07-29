e8ca5d6e315eb61380bac43fb3bcb4ee31734569
andyteichner <40463426+andyteichner@users.noreply.github.com>
2018-07-29 18:55:53 +0200

    Die Main Page ohne Fragment bis jetzt.

--- app/src/main/java/com/example/andy/thankyoujames/MainJames.java
+++ app/src/main/java/com/example/andy/thankyoujames/MainJames.java
@@ -1,35 +1,67 @@
 package com.example.andy.thankyoujames;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainJames extends AppCompatActivity {
+public class MainJames extends Activity implements View.OnClickListener {
 
-    String testString = "string";
-
-
-    String warumgehstduned = "WTF";
-
+    private ImageView   headerImage;
+    private TextView    headerText, offerText;
+    private Button      headerBurger, headerShopping;
+    private ImageButton offerOne, offerTwo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_james);
-        helloWorld();
+        initView();
+
     }
 
-    //Hallo#
+    private void initView(){
+        // Buttons
+        headerBurger = findViewById(R.id.header_burger_button);
+        headerShopping = findViewById(R.id.header_shopping_button);
+        headerBurger.setOnClickListener(this);
+        headerBurger.setOnClickListener(this);
 
-    private void helloWorldNummerZwei(){
-        
+        // ImageViews
+        headerImage = findViewById(R.id.header_image);
+
+        // TextViews
+        headerText = findViewById(R.id.header_text);
+        offerText = findViewById(R.id.main_james_offer_text);
+
+        // ImageButtons
+        offerOne = findViewById(R.id.main_james_offer_one);
+        offerTwo = findViewById(R.id.main_james_offer_two);
+        offerOne.setOnClickListener(this);
+        offerTwo.setOnClickListener(this);
+
+
     }
 
-    private void helloWorldDieDritte(){
-        
-    }
-
-    private void helloWorld(){
+    @Override
+    public void onClick(View view) {
+        // TODO: 29.07.2018  intents für die Angebote sowie das Fragment und den Warenkorb
+        switch (view.getId()){
+            case R.id.header_burger_button:
+                break;
+            case R.id.header_shopping_button:
+                break;
+            case R.id.main_james_offer_one:
+                break;
+            case R.id.main_james_offer_two:
+                break;
+        }
 
     }
 }
