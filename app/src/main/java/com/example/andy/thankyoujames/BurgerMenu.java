e5fbda581acee29b3dd70bd2d5b204ef97947de8
andyteichner <40463426+andyteichner@users.noreply.github.com>
2018-08-07 11:15:48 +0200

    Funktion mit Fragment. Nächster Schritt -> auf und zu des Fragments

--- app/src/main/java/com/example/andy/thankyoujames/BurgerMenu.java
+++ app/src/main/java/com/example/andy/thankyoujames/BurgerMenu.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,4 +53,23 @@ public class BurgerMenu extends Fragment {
 
         return v;
     }
+
+
+
+
+    public void showHideFragment(final Fragment fragment){
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.setCustomAnimations(android.R.animator.fade_in,
+                android.R.animator.fade_out);
+
+        if (fragment.isHidden()) {
+            ft.show(fragment);
+        } else {
+            ft.hide(fragment);
+        }
+        ft.commit();
+    }
+
+
 }
